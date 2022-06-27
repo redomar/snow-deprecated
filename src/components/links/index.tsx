3336027445325db664d9ed396ccbb5ecc715f632
Mohamed Omar <Mohamed12@live.co.uk>
2022-06-27 23:14:19 +0100

    :sparkles:  modularise links in component

--- src/components/links/index.tsx
+++ src/components/links/index.tsx
@@ -1,29 +1,26 @@
 import { useRouter } from "next/router";
 
-type LinkProps = {
+export type LinkComponentProps = {
+  id: string;
   linkTitle: string;
   linkUrl: string;
   linkHandle?: string;
   active?: boolean;
 };
 
-export const Links = () => {
+const Links = ({ linkProps }: { linkProps?: LinkComponentProps[] }) => {
   return (
     <div className="h-screen flex">
-      <div className="w-64 px-8 py-4 bg-zinc-900/50 border-r border-zinc-900/80">
+      <div className="flex flex-col gap-8 w-64 px-8 py-4 bg-zinc-900/50 border-r border-zinc-900/80">
         {/* Logo Img */}
-        <nav className="mt-8">
-          <h2 className="text-xs font-semibold  text-zinc-400 uppercase tracking-wider">
-            Social Links
-          </h2>
-          <Link
-            active={true}
-            linkTitle="Twitter"
-            linkUrl="https://twitter.com/rxemar1"
-            linkHandle="@rxemar1"
-          />
-          <Link linkTitle="website" linkUrl="https://rx1.uk" linkHandle="www.rx1.uk" />
-        </nav>
+        <LinkSection sectionTitle="Social Links">
+          {linkProps?.map((link) => (
+            <Link key={link.linkTitle} {...link} />
+          ))}
+        </LinkSection>
+        <LinkSection sectionTitle="Projects" className="text-sm text-zinc-500 tracking-widest">
+          ...
+        </LinkSection>
       </div>
       <div className="flex-1 min-w-0 ">
         <div className="border-b-2 border-zinc-900"></div>
@@ -32,7 +29,26 @@ export const Links = () => {
   );
 };
 
-function Link({ active = false, ...linkProps }: LinkProps): JSX.Element {
+export function LinkSection({
+  children,
+  sectionTitle,
+  className = "",
+}: {
+  children?: React.ReactNode;
+  sectionTitle: string;
+  className?: string;
+}) {
+  return (
+    <div className={className}>
+      <h2 className="text-xs font-semibold  text-zinc-400 uppercase tracking-wider">
+        {sectionTitle}
+      </h2>
+      {children}
+    </div>
+  );
+}
+
+function Link({ active = false, ...linkProps }: LinkComponentProps): JSX.Element {
   const router = useRouter();
 
   const onClick = (e: React.MouseEvent<HTMLButtonElement>) => {
@@ -43,11 +59,9 @@ function Link({ active = false, ...linkProps }: LinkProps): JSX.Element {
   return (
     <div className={`my-2 -mx-3 text-white ${active ? "font-bold" : "font-light"}`}>
       <button
-        className={`flex w-full justify-between items-center px-3 py-2 ${
-          active
-            ? "bg-zinc-700 border border-zinc-500"
-            : "text-zinc-500 bg-zinc-800/30 tracking-wide"
-        } hover:bg-zinc-600 hover:cursor-pointer hover:text-white rounded-sm`}
+        className={`flex w-full justify-between items-baseline px-3 py-2 ${
+          active ? "bg-zinc-700/80 border border-zinc-600" : "text-zinc-500 tracking-wide"
+        } hover:bg-zinc-700 hover:cursor-pointer hover:text-white rounded-lg`}
         onClick={onClick}>
         <span className="text-sm">{linkProps.linkTitle}</span>
         {linkProps.linkHandle && (
@@ -57,3 +71,5 @@ function Link({ active = false, ...linkProps }: LinkProps): JSX.Element {
     </div>
   );
 }
+
+export default Links;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,16 @@
-import { Links } from "@/components/links";
+import Links, { LinkComponentProps } from "@/components/links";
 import { trpc } from "@/utils/trpc";
 
 export default function Home() {
   const { data, isLoading } = trpc.useQuery(["hello", { text: "world" }]);
 
+  const link: LinkComponentProps = {
+    id: "link1",
+    active: false,
+    linkTitle: "Twitter",
+    linkUrl: "https://twitter.com/rxemar1",
+    linkHandle: "@rxemar1",
+  };
   // return <div className="text-4xl">Mr {data?.greeting}</div>;
-  return <Links />;
+  return <Links linkProps={[link]} />;
 }
